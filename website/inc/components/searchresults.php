e9abaab772a881c4245495a714173357acce1f46
LiamOsler <liam.osler@dal.ca>
2021-12-22 05:13:09 -0400

    prepare search results for card pagination

--- website/inc/components/searchresults.php
+++ website/inc/components/searchresults.php
@@ -3,10 +3,55 @@
   include "../../db/db.php";
 ?>
 
+<div class="container">
+  
+    <div class="card-deck mt-4">
+        <div class="card text-center">
+            <div class="card-block">
+                <h4 class="card-title">Funding Options</h4>
+                <p class="card-text">
+                    Add credit/debit card
+                    <br> Top up Account
+                    <br> Manage cards
+                </p>
+            </div>
+            <div class="card-footer">
+                Link
+            </div>
+        </div>
+
+        <div class="card text-center">
+            <div class="card-block">
+                <h4 class="card-title">Account History</h4>
+                <p class="card-text">
+                    View all financial transactions on my account
+                </p>
+            </div>
+            <div class="card-footer">
+                Link
+            </div>
+        </div>
+        <div class="card text-center">
+            <div class="card-block">
+                <h4 class="card-title">User Settings</h4>
+                <p class="card-text">
+                    Edit personal details
+                    <br> Change top-up settings
+                    <br> Change password
+                </p>
+            </div>
+            <div class="card-footer">
+                Link
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <div class="container-fluid">
 <br>
 <p>Restaurants:</p>
-<div class="row">
+<div class="card-deck mt-4">
   <?php
     // If the search query string is set
     if (isset($_GET["search-query"])) {
@@ -37,19 +82,23 @@
           $r_hour_close = $current["r_hour_close"];
           $r_rating = $current["r_rating"];
           ?>
-          <div class = "col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2">
-            <?php echo $r_id;?>
-              <img class="card-img-top" src="images/hotsauce.png" alt="Card image cap">
-              <div class="card-body">
-              <h5 class="card-title"><?php echo $r_name;?></h5>
-              <p class="card-text"><?php echo $r_category;?></p>
-              </div>
-          </div>
+            <div class="card h-100 text-center">
+              <?php echo $r_id;?>
+                <img class="card-img-top" src="images/hotsauce.png" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title"><?php echo $r_name;?></h5>
+                <p class="card-text"><?php echo $r_category;?></p>
+                </div>  
+                <div class="card-footer mt-auto">
+                  <button type="button" class="btn btn-success">View Menu</button>
+                </div>
+            </div>
           <?php
         }
       }
   ?>
-  </div>
+</div>
+</div>
 
   <div class="row">
     <?php
@@ -57,12 +106,7 @@
               INNER JOIN restaurants ON menu_items.r_id = restaurants.r_id
               WHERE m_name LIKE '%{$searchString}%'";
 
-    echo mysqli_num_rows($results);
-
     $results = $dbconn->query($query);
-
-
-
     if (mysqli_num_rows($results) == 0) {
       echo "<p>This search returned no results.</p>";
     }
