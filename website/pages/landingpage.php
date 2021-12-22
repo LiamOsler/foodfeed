27e91f8a781f20db261407c534afefb7e631cd49
LiamOsler <liam.osler@dal.ca>
2021-12-21 20:32:42 -0400

    format landing page to better use search

--- website/pages/landingpage.php
+++ website/pages/landingpage.php
@@ -1,5 +1,14 @@
 <div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
+  <h1 class="display-4">Find Restaurants and Menu Items:</h1>
+  <div class="card" style="">
+      <div class="card-body">
+          <p class="card-text">
+          <?php include '../inc/components/searcharea.php'; ?> 
+          </p>
+      </div>
+  </div>
+
+
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
@@ -9,13 +18,5 @@
 </div>
 
 <div class="col-md-12 lean-canvas">
-  <div class="card" style="">
-      <div class="card-body">
-          <h5 class="card-title"> <i class="bi bi-shop"></i> Store Item Demo</h5>
-          <p class="card-text">
-          Search for an item:
-          <?php include '../inc/components/searcharea.php'; ?> 
-          </p>
-      </div>
-  </div>
+
 </div>
