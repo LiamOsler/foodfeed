ee7bd63880123c1a0513fbcdd2912c90f3b09f12
LiamOsler <liam.osler@dal.ca>
2021-12-21 20:07:32 -0400

    basic site structure with landing page / navbar

--- website/index.php
+++ website/index.php
@@ -1,31 +1,7 @@
 <?php include "inc/header.php" ?>
-
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div>
-
-<script>
-//Scripts Relating to the PHP navigation:
-$(document).ready(function(){
-  $("button").click(function(){
-    $.ajax({url: "pages/cart.php", success: function(result){
-      $("#div1").html(result);
-    }});
-  });
-});
-</script>
 </head>
 <body>
 
-<div id="div1"><h2>Let jQuery AJAX Change This Text</h2></div>
-
-<button>Get External Content</button>
-
+<div id="page-content"></div>
 
 <?php include "inc/footer.php" ?>
