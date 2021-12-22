c237bc14193aaa90b38c22a00a6cd97306285156
LiamOsler <liam.osler@dal.ca>
2021-12-22 17:52:58 -0400

    give bootstrap cards consistent height and improve search results page

--- website/inc/components/searchresults.php
+++ website/inc/components/searchresults.php
@@ -3,55 +3,10 @@
   include "../../db/db.php";
 ?>
 
-<div class="container">
-  
-    <div class="card-deck mt-4">
-        <div class="card text-center">
-            <div class="card-block">
-                <h4 class="card-title">Funding Options</h4>
-                <p class="card-text">
-                    Add credit/debit card
-                    <br> Top up Account
-                    <br> Manage cards
-                </p>
-            </div>
-            <div class="card-footer">
-                Link
-            </div>
-        </div>
-
-        <div class="card text-center">
-            <div class="card-block">
-                <h4 class="card-title">Account History</h4>
-                <p class="card-text">
-                    View all financial transactions on my account
-                </p>
-            </div>
-            <div class="card-footer">
-                Link
-            </div>
-        </div>
-        <div class="card text-center">
-            <div class="card-block">
-                <h4 class="card-title">User Settings</h4>
-                <p class="card-text">
-                    Edit personal details
-                    <br> Change top-up settings
-                    <br> Change password
-                </p>
-            </div>
-            <div class="card-footer">
-                Link
-            </div>
-        </div>
-    </div>
-</div>
-
-
 <div class="container-fluid">
+<div class = "row">
 <br>
 <p>Restaurants:</p>
-<div class="card-deck mt-4">
   <?php
     // If the search query string is set
     if (isset($_GET["search-query"])) {
@@ -82,17 +37,18 @@
           $r_hour_close = $current["r_hour_close"];
           $r_rating = $current["r_rating"];
           ?>
-            <div class="card h-100 text-center">
-              <?php echo $r_id;?>
-                <img class="card-img-top" src="images/hotsauce.png" alt="Card image cap">
-                <div class="card-body">
-                <h5 class="card-title"><?php echo $r_name;?></h5>
-                <p class="card-text"><?php echo $r_category;?></p>
-                </div>  
-                <div class="card-footer mt-auto">
-                  <button type="button" class="btn btn-success">View Menu</button>
-                </div>
-            </div>
+          <div class = "col-sm-6 col-md-4 col-lg-3 col-xl-2 py-20 mb-4">
+              <div class="card h-100 text-center">
+                  <img class="card-img-top" src="images/hotsauce.png" alt="Card image cap">
+                  <div class="card-body card-search-results">
+                  <h5 class="card-title"><?php echo $r_name;?></h5>
+                  <p class="card-text"><?php echo $r_category;?></p>
+                  </div>  
+                  <div class="card-footer mt-auto">
+                    <button type="button" class="btn btn-success">View Menu</button>
+                  </div>
+              </div>
+          </div>
           <?php
         }
       }
