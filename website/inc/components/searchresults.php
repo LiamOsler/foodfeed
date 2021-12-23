6f5d233c7e8229107829013f01b2f1ea8892f60b
LiamOsler <liam.osler@dal.ca>
2021-12-23 18:35:27 -0400

    add imatges to search results

--- website/inc/components/searchresults.php
+++ website/inc/components/searchresults.php
@@ -1,159 +1,44 @@
 
-<?php
-  include "../../db/db.php";
-?>
+<?php include "../../db/db.php";?>
 <div id = "scroller">
-  <section class="">
-      <div class="row">
-          <div class="col-6">
-              <h3 class="mb-3 display-6">Results: </h3>
-          </div>
-          <div class="col-6 text-right">
-              <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-slide="prev">
-                  <i class="bi bi-arrow-left"></i>
-              </a>
-              <a class="btn btn-primary mb-3 " href="#carouselExampleIndicators2" role="button" data-slide="next">
-                  <i class="bi bi-arrow-right"></i>
-              </a>
-          </div>
-          <div class="col-12">
-              <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+    <section class="">
+        <div class="row">
+            <div class="col-6">
+                <h3 class="mb-3 display-6">Results: </h3>
+            </div>
 
-                  <div class="carousel-inner">
-                      <div class="carousel-item active">
-                          <div class="row">
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
+            <div class="col-6 text-right">
+                <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+                    <i class="bi bi-arrow-left"></i>
+                </a>
+                <a class="btn btn-primary mb-3 " href="#carouselExampleIndicators2" role="button" data-slide="next">
+                    <i class="bi bi-arrow-right"></i>
+                </a>
+            </div>
+<?php
+    // If the search query string is set
+if (isset($_GET["search-query"])) {
+    $searchString = $_GET["search-query"];
 
-                      <div class="carousel-item">
-                          <div class="row">
+    $query = "SELECT * FROM `restaurants`
+                WHERE r_name LIKE '%{$searchString}%'";
 
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532771098148-525cefe10c23?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3f317c1f7a16116dec454fbc267dd8e4">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
-                                      </div>
-
-                                  </div>
-                              </div>
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532715088550-62f09305f765?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=ebadb044b374504ef8e81bdec4d0e840">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1506197603052-3cc9c3a201bd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=0754ab085804ae8a3b562548e6b4aa2e">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="carousel-item">
-                          <div class="row">
-
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=ee8417f0ea2a50d53a12665820b54e23">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532777946373-b6783242f211?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=8ac55cf3a68785643998730839663129">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="col-md-4 mb-3">
-                                  <div class="card">
-                                      <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532763303805-529d595877c5?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=5ee4fd5d19b40f93eadb21871757eda6">
-                                      <div class="card-body">
-                                          <h4 class="card-title">Special title treatment</h4>
-                                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </section>
-</div>
-
-  <div class = "row">
-  <p></p>
-  <h1 class = "display-6">Results:</h1>
-    <?php
-      // If the search query string is set
-      if (isset($_GET["search-query"])) {
-        $searchString = $_GET["search-query"];
-
-        $query = "SELECT * FROM `restaurants`
-                  WHERE r_name LIKE '%{$searchString}%'";
-
-        $results = $dbconn->query($query);
-        
-        if (mysqli_num_rows($results) == 0) {
-          echo "<p>This search returned no results.</p>";
-        }
-        else { 
-          while ($current= $results->fetch_assoc()){
+    $results = $dbconn->query($query);
+    
+    if (mysqli_num_rows($results) == 0) {
+        echo "<p>This search returned no results.</p>";
+    }
+    else {
+        $resultCount = 0;
+        ?>
+        <div class="col-12">
+            <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+        <?php
+        while ($current= $results->fetch_assoc()){
+            $resultCount++;
             $r_id = $current["r_id"];
             $r_name = $current["r_name"];
             $r_phone = $current["r_phone"];
@@ -168,22 +53,46 @@
             $r_hour_open = $current["r_hour_open"];
             $r_hour_close = $current["r_hour_close"];
             $r_rating = $current["r_rating"];
+            $r_img = $current["r_img"];
             ?>
-            <div class = "col-sm-6 col-md-4 col-lg-3 col-xl-2 py-20 mb-4">
-                <div class="card h-100 text-center">
-                    <img class="card-img-top" src="images/hotsauce.png" alt="Card image cap">
-                    <div class="card-body card-search-results">
-                    <h5 class="card-title"><?php echo $r_name;?></h5>
-                    <p class="card-text"><?php echo $r_category;?></p>
-                    </div>  
-                    <div class="card-footer mt-auto">
-                      <a id = "#test" type="button" class="btn btn-success" onclick = "viewRestaurant(<?php echo $r_id?>, '<?php echo $searchString?>' )">View Menu</a>
+                            <div class = "col-md-3 mb-3">
+                                <div class="card h-100 text-center card-restaurant">
+                                <div class="img" style="background-image:url('images/restaurants/<?php echo $r_img;?>')"></div>
+                                    <!-- <img class="card-img-top" src="images/restaurants/<?php echo $r_img;?>" alt="Card image cap"> -->
+                                    <div class="card-body card-search-results">
+                                    <h5 class="card-title"><?php echo $r_name;?></h5>
+                                    <p class="card-text"><?php echo $r_category;?></p>
+                                    </div>  
+                                    <div class="card-footer mt-auto">
+                                    <a id = "#test" type="button" class="btn btn-success" onclick = "viewRestaurant(<?php echo $r_id?>, '<?php echo $searchString?>' )">View Menu</a>
+                                    </div>
+                                </div>
+                            </div>
+            <?php
+            if($resultCount % 4 == 0){              
+            ?>
+                        </div>
                     </div>
+                    <div class="carousel-item">
+                        <div class="row">
+            <?php
+            }
+            if($resultCount == mysqli_num_rows($results)){
+                ?>
                 </div>
             </div>
             <?php
-          }
+            }
         }
-      }
-    ?>
-  </div>
+        ?>
+
+        <?php
+    }
+}
+?>              
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
