db8de6376f5c667465393066fac59b9681f19296
LiamOsler <liam.osler@dal.ca>
2021-12-24 02:21:48 -0400

    Add filler content to landing page "about section"

--- website/pages/landingpage.php
+++ website/pages/landingpage.php
@@ -1,6 +1,5 @@
-<div class="jumbotron jumbotron-search">
 
-<div class="container-fluid">
+<div class="container-xl">
       <div class="row">
           <div class="col-12">
               <div class="card">
@@ -14,10 +13,32 @@
                       <?php include '../inc/components/toprated.php'; ?> 
                       <br>
                       <h5 class="card-title display-4"><i class="bi bi-tags"></i> Popular Tags:</h5>
-                      <?php include '../inc/components/tagcloud.php'; ?> 
+                      <?php include '../inc/components/tagcloud.php'; ?>
+                      <br> 
 
                       <h5 class="card-title display-4"><i class="bi bi-patch-question"></i> About Food Feed:</h5>
-                      <p>Hello</p>
+                      
+                      <div class="card-group ">
+                          <div class="card ">
+                            <div class="card-body">
+                              <h5 class="card-title">Food Feed helps you save time</h5>
+                              <p class="card-text">There are dozens of third-party food delivery services like UberEats, DoorDash, GrubHub - in some cases restaurants may even use multiple delivery services. Currently, if a consumer wants to compare the final price of an order on multiple delivery services, they must go through the tedious process of finding the restaurant on each platform and proceeding through their respective checkout processes.</p>
+                            </div>
+                          </div>
+                          <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                          </div>
+                          <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                          </div>
+                      </div>
+
 
                   </div>
               </div>
