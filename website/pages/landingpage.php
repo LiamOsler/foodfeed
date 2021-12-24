e485c6b7fc2d2923d38a3bc07a6212507d90f420
LiamOsler <liam.osler@dal.ca>
2021-12-24 02:50:53 -0400

    remove extra cards from landingpage area

--- website/pages/landingpage.php
+++ website/pages/landingpage.php
@@ -21,20 +21,8 @@
                       <div class="card-group ">
                           <div class="card ">
                             <div class="card-body">
-                              <h5 class="card-title">Food Feed helps you save time</h5>
-                              <p class="card-text">There are dozens of third-party food delivery services like UberEats, DoorDash, GrubHub - in some cases restaurants may even use multiple delivery services. Currently, if a consumer wants to compare the final price of an order on multiple delivery services, they must go through the tedious process of finding the restaurant on each platform and proceeding through their respective checkout processes.</p>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-body">
-                              <h5 class="card-title">Card title</h5>
-                              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-body">
-                              <h5 class="card-title">Card title</h5>
-                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                              <h5 class="card-title">Food Feed helps you save time and money</h5>
+                              <p class="card-text">There are dozens of third-party food delivery services like UberEats, DoorDash, GrubHub - in some cases restaurants may even use multiple delivery services. Currently, if a consumer wants to compare the final price of an order on multiple delivery services, they must go through the tedious process of finding the restaurant on each platform and proceeding through their respective checkout processes. Food Feed helps you find and compare prices across platforms.</p>
                             </div>
                           </div>
                       </div>
@@ -45,5 +33,4 @@
             </div>
           </div>
       </div>
-</div>
 
