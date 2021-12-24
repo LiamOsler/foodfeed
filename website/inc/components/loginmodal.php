7d5640dd67d0fbad7d5f5800fadb50136bae7a65
LiamOsler <liam.osler@dal.ca>
2021-12-24 04:13:34 -0400

    seperated the login modal and newaccountmodal

--- website/inc/components/loginmodal.php
+++ website/inc/components/loginmodal.php
@@ -16,21 +16,4 @@
 		</div>
 	</div>
 </div>
-<!-- Account Creation Modal-->
-<div class="modal fade" id="newAccountModal" tabindex="-1" role="dialog" aria-labelledby="newAccountModalLabel" aria-hidden="true">
-	<div class="modal-dialog modal-xl" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h4 class="modal-title" id="newAccountModalLabel"> <i class="bi-person-plus"></i> Create an Account</h4>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<div class="modal-body">
-				<?php
-					include 'inc/components/userregistrationform.php';
-				?>
-			</div>
-		</div>
-	</div>
-</div>
+
